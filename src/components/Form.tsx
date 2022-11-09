f90c3da0a90168930635e1d2c7deffb9fe28b0ff
morimoto-haruhi <morimoto.haruhi@gmail.com>
2022-11-09 15:37:21 +0900

    www.weatherapi.comから天気情報を検索できるようにした

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,10 +1,18 @@
 // Form.tsx
+import { useState } from "react";
 
 const Form = () => {
+    const [city, setCity] = useState("");
+    const getWeather = (e: any) => {
+        e.preventDefault();
+        fetch("https://api.weatherapi.com/v1/current.json?key=58f47afa84364dadb4393017212606&q=London&aqi=no")
+            .then(res => res.json())
+            .then(data => console.log(data))
+    }
     return (
         <form>
-            <input type="text" name="city" placeholder="都市名"/>
-            <button type="submit">Get Weather</button>
+            <input type="text" name="city" placeholder="都市名" onChange={e => setCity(e.target.value)}/>
+            <button type="submit" onClick={getWeather} >Get Weather</button>
         </form>
     );
 };
