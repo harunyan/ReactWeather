1a7dee6003ce1f9d2e2a8a8dec72cf863481963c
morimoto-haruhi <morimoto.haruhi@gmail.com>
2022-11-25 11:19:03 +0900

    第２章の天気取得表示アプリを完成

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,48 @@
-import React from 'react';
+import React, {useState} from 'react';
 import Title from "./components/Title";
 import Form from "./components/Form";
+import Results from "./components/Results";
 import './App.css';
 
+type ResultsStateType = {
+    country: string;
+    cityName: string;
+    temperature: string;
+    conditionText: string;
+    icon: string;
+}
+
 function App() {
+    const [city, setCity] = useState("");
+    const [results, setResults] = useState<ResultsStateType>({
+        country: "",
+        cityName: "",
+        temperature: "",
+        conditionText: "",
+        icon: "",
+    });
+
+    const getWeather = (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+        fetch(`https://api.weatherapi.com/v1/current.json?key=58f47afa84364dadb4393017212606&q=${city}&aqi=no`)
+            .then(res => res.json())
+            .then(data => {
+                setResults({
+                    country: data.location.country,
+                    cityName: data.location.name,
+                    temperature: data.current.temp_c,
+                    conditionText: data.current.condition.text,
+                    icon: data.current.condition.icon,
+                })
+            })
+    }
   return (
-    <div className="App">
-        <Title />
-        <Form />
+    <div className="wrapper">
+        <div className="container">
+            <Title />
+            <Form setCity={setCity} getWeather={getWeather} />
+            <Results results={results} />
+        </div>
     </div>
   );
 }
---
+++ src/components/Results.tsx
@@ -0,0 +1,27 @@
+// Results.tsx
+
+type ResultsPropType = {
+    results: {
+        country: string;
+        cityName: string;
+        temperature: string;
+        conditionText: string;
+        icon: string;
+    }
+}
+
+const Results = (props: ResultsPropType) => {
+    return(
+        <div>
+            {props.results.country && <div>{props.results.country}</div>}
+            {props.results.cityName && <div>{props.results.cityName}</div>}
+            {props.results.temperature && <div>{props.results.temperature}<span>℃</span></div>}
+            {props.results.conditionText && <div>
+                <img src={props.results.icon} alt="icon" />
+                <span>{props.results.conditionText}</span>
+            </div>}
+        </div>
+    );
+};
+
+export default Results;
